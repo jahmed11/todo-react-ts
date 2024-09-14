4ec3b7fdc55a4b3f10e16c31ae8a1720356e583e
Junaid Ahmed <junaid@smilegeniusdental.com>
2024-09-14 14:51:03 +0100

    spell fix

--- src/pages/todo/components/displayTodo/DisplayTodo.tsx
+++ src/pages/todo/components/displayTodo/DisplayTodo.tsx
@@ -1,4 +1,5 @@
 import { useAppSelector, useAppDispatch } from "hooks/reduxHooks";
+import { useState, useEffect } from "react";
 import { deleteTodo } from "pages/todo/reducer/todoReducer";
 import { todoItemsSelector } from "pages/todo/reducer/selectors";
 import Todo from "./Todo";
@@ -8,17 +9,26 @@ import { TodoProps } from "types/todo";
 const DisplayTodo = () => {
   const dispatch = useAppDispatch();
   const todoItems = useAppSelector(todoItemsSelector);
+  const [sortedTodos, setSortedTodos] = useState<TodoProps[]>([]);
 
   const onTodoDeleteClick = (id: number) => {
     dispatch(deleteTodo(id));
   };
+
+  useEffect(() => {
+    const copy = JSON.parse(JSON.stringify(todoItems)) as TodoProps[];
+    const sortedItems = copy.sort((a, b) => {
+      return a.text.localeCompare(b.text);
+    }) as TodoProps[];
+    setSortedTodos(sortedItems);
+  }, [todoItems]);
 
   const renderTodoList = (items: TodoProps[]) => {
     if (!items.length) {
       return <div className={styles["todos-list--empty"]}>Please Add your todos</div>;
     }
 
-    return todoItems.map((todoItem: TodoProps, index: number) => {
+    return items.map((todoItem: TodoProps, index: number) => {
       return (
         <Todo
           key={todoItem.id}
@@ -34,7 +44,7 @@ const DisplayTodo = () => {
   return (
     <div className={styles["todos-container"]}>
       <h2>Todo List</h2>
-      <div className={styles["todos-list"]}>{renderTodoList(todoItems)}</div>
+      <div className={styles["todos-list"]}>{renderTodoList(sortedTodos)}</div>
     </div>
   );
 };
